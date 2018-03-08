34a23e64b23351bd1e5223f8ff20839cbe4d1e17
Nolan Byrnes <nolanehs@gmail.com>
2018-03-08 11:35:43 +0000

    automatically backed up by learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -7,7 +7,6 @@ class RecipesController < ApplicationController
 
   def create
     @recipe = Recipe.create(recipe_params)
-    @recipe = Ingredient.create(ingredient_params)
     if @recipe.save
       redirect_to recipe_path(@recipe.id)
     else
