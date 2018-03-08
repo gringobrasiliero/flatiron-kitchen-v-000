85f1a4bc23ebdccce5a7555bec1a71d7f937998e
Nolan Byrnes <nolanehs@gmail.com>
2018-03-08 09:59:01 +0000

    automatically backed up by learn

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,12 +1,23 @@
 class IngredientsController < ApplicationController
 
+def new
+  @ingredient = Ingredient.new
+  @ingredients = Ingredient.all
+end
+
 def create
 @ingredient = Ingredient.create(ingredient_params)
+redirect_to new_ingredient_path
 end
 
 def edit
 
 end
 
+private
+
+def ingredient_params
+
+end
 
 end
