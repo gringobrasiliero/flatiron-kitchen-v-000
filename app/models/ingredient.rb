fd1a91f05349ef6f23bc1da51a9e2fbf87d21b6e
Nolan Byrnes <nolanehs@gmail.com>
2018-03-08 10:18:12 +0000

    automatically backed up by learn

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,5 +1,5 @@
 class Ingredient < ActiveRecord::Base
   validates :name, presence: true
-  belongs_to :recipe
   has_many :recipes, through: :recipe_ingredients
+  has_many :recipe_ingredients
 end
