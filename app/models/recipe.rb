0630c71124996ef8883904e4f448dce557960f7e
Nolan Byrnes <nolanehs@gmail.com>
2018-03-08 10:18:00 +0000

    automatically backed up by learn

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,5 +1,5 @@
 class Recipe < ActiveRecord::Base
   validates :name, presence: true
-  has_many :ingredients
    has_many :ingredients, through: :recipe_ingredients
+   has_many :recipe_ingredients
 end
